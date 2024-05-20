8c4a92ea5673f3ff7ca7354983f9e296aabeb086
bryokn <bryokn@gmail.com>
2024-05-20 15:40:12 +0300

    added placeholders

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,33 +1,3 @@
-// import React, { useState, useEffect } from 'react';
-// import './calculator.css';
-
-// const PriceCalculatorForm = ({ isOpen, onClose }) => {
-//   const [distance, setDistance] = useState('');
-//   const [fragility, setFragility] = useState('');
-//   const [estimatedPrice, setEstimatedPrice] = useState(null);
-//   const modalOverlayRef = React.useRef(null);
-
-//   useEffect(() => {
-//     const modalOverlay = modalOverlayRef.current;
-
-//     if (modalOverlay) {
-//       if (isOpen) {
-//         modalOverlay.classList.add('open');
-//       } else {
-//         modalOverlay.classList.remove('open');
-//       }
-//     }
-//   }, [isOpen]);
-
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     // Calculate the estimated price based on distance and fragility
-//     const calculatedPrice = calculatePrice(distance, fragility);
-//     setEstimatedPrice(calculatedPrice);
-//   };
-
-
-
 import React, { useState, useEffect } from 'react';
 import './calculator.css';
 
@@ -152,6 +122,7 @@ return (
             <input
               type="number"
               id="weight"
+              placeholder="What do you estimate the weight to be?"
               value={weight}
               onChange={(e) => setWeight(e.target.value)}
               required
@@ -167,6 +138,7 @@ return (
             <input
               type="number"
               id="distance"
+              placeholder="What do you estimate the distance to be?"
               value={distance}
               onChange={(e) => setDistance(e.target.value)}
               required
@@ -265,90 +237,3 @@ return (
 };
 
 export default PriceCalculatorForm;
-
-
-//   const calculatePrice = (distance, fragility) => {
-//   let basePrice;
-//   let additionalPricePerKm;
-
-//   // Determine base price based on fragility
-//   if (fragility === 'low') {
-//     basePrice = 300; // Base price for low fragility items
-//     additionalPricePerKm = 50; // Additional price per km after 1 km
-//   } else if (fragility === 'medium') {
-//     basePrice = 500; // Base price for medium fragility items
-//     additionalPricePerKm = 80; // Additional price per km after 1 km
-//   } else if (fragility === 'high') {
-//     basePrice = 700; // Base price for high fragility items
-//     additionalPricePerKm = 120; // Additional price per km after 1 km
-//   } else {
-//     basePrice = 300; // Default base price for any item
-//     additionalPricePerKm = 0; // No additional price per km
-//   }
-
-//   // Calculate price based on distance
-//   let distancePrice;
-//   if (distance <= 1) {
-//     distancePrice = 0; // No additional price for the first kilometer
-//   } else {
-//     distancePrice = (distance - 1) * additionalPricePerKm; // Additional price for distance beyond 1 km
-//   }
-
-//   const totalPrice = basePrice + distancePrice;
-//   return totalPrice.toFixed(2); // Return the calculated price rounded to 2 decimal places
-// };
-
-//   return (
-//     <div className={`price-calculator-modal ${isOpen ? 'open' : ''}`}>
-//       <div className="price-calculator-modal-content">
-//         <span className="price-calculator-close" onClick={onClose}>
-//           &times;
-//         </span>
-//         <h2>Get A Quote:</h2><br />
-//         <form onSubmit={handleSubmit}>
-//           <div>
-//             <label htmlFor="distance"><b>What is the distance? (in km):</b></label><br /><br />
-//             <input
-//               type="number"
-//               id="distance"
-//               // placeholder="how far is it?"
-//               value={distance}
-//               onChange={(e) => setDistance(e.target.value)}
-//               required
-//             />
-//           </div>
-//           <br />
-//           <div>
-//             <label htmlFor="fragility"><b>How fragile are the items?</b></label><br /><br />
-//             <select
-//               id="fragility"
-//               value={fragility}
-//               onChange={(e) => setFragility(e.target.value)}
-//               required
-//             >
-//               <option value="">Select Fragility</option>
-//               <option value="low">Low</option>
-//               <option value="medium">Medium</option>
-//               <option value="high">High (Glass) </option>
-//             </select>
-//           </div>
-//           {/* <br /> <br /> */}
-//           {estimatedPrice !== null && (
-//           <div className="estimated-price">
-//             <p>Estimated Price: {estimatedPrice} Ksh</p>
-//           </div>
-//         )} 
-//         <br />
-//           <div className="calculate-button-container">
-//           <button type="submit" className="calculate-button">Calculate Price</button><br />
-//           <button type="button" className="create-order-button"><a href="/new-delivery-order">Create Order</a></button>
-//           </div>
-//           {/* <br /><br /><br /> */}
-          
-//         </form>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default PriceCalculatorForm;
