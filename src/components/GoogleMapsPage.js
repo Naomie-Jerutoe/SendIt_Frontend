71e53323a179696ce1af14f332a02720fbb6cfeb
Levis Ngigi <ngigilevis@gmail.com>
2024-05-21 19:49:00 +0300

    feat:update styling

--- src/components/GoogleMapsPage.js
+++ src/components/GoogleMapsPage.js
@@ -5,6 +5,7 @@ const GoogleMapsPage = () => {
   return (
     <div className="maps-page">
       <h1>Google Maps Page</h1>
+      <p>To see estimated travel distance and duration, click on the marker.</p>
       <MapWithGeocoding /> {/* Render your Google Maps component here */}
     </div>
   );
