09cd28f42dc3790d7fc410ceaeeb8d8b0391a8d5
ewkimwaki <e.w.kimwaki@gmail.com>
2024-05-21 11:27:33 +0300

    fixed repeated routes

--- src/index.js
+++ src/index.js
@@ -15,6 +15,10 @@ import ContactUs from './views/contact-us'
 import Signup from './views/signup'
 import Home from './views/home'
 import NotFound from './views/not-found'
+import Dashboard from './components/Admin UI/Dashboard/Dashboard'
+import Users from './components/Admin UI/Users/Users'
+import Parcels from './components/Admin UI/Parcels/Parcels'
+import Orders from './components/Admin UI/Orders/Orders'
 
 const App = () => {
   return (
@@ -22,13 +26,10 @@ const App = () => {
       <Switch>
         <Route component={Services} exact path="/services" />
         <Route component={UserDashboard} exact path="/user-dashboard" />
-        <Route component={Services} exact path="/services" />
-        <Route component={UserDashboard} exact path="/user-dashboard" />
         <Route component={AboutPage} exact path="/about-page" />
         <Route component={ContactUs} exact path="/contact-us" />
         <Route component={Signup} exact path="/signup" />
         <Route component={Home} exact path="/" />
-        <Route component={SignUp} exact path="/signup" />
         <Route component={Dashboard} exact path="/admin_dashboard" />
         <Route component={Users} exact path="/admin_users" />
         <Route component={Parcels} exact path="/admin_parcels" />
