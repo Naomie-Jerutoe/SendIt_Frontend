dff59b0efd37d4134a280509987ee2bcc81303e8
Naomi Lagat <naomiejerutoe@gmail.com>
2024-05-22 11:28:11 +0300

    update changes

--- src/index.js
+++ src/index.js
@@ -17,6 +17,10 @@ import Dashboard from "./components/Admin UI/Dashboard/Dashboard";
 import Users from "./components/Admin UI/Users/Users";
 import Parcels from "./components/Admin UI/Parcels/Parcels";
 import Orders from "./components/Admin UI/Orders/Orders";
+import Services from "./views/services";
+import UserDashboard from "./views/user-dashboard";
+import NewDeliveryOrder from "./views/NewDeliveryOrder";
+import DeliveryOrderSummary from "./views/DeliveryOrderSummary";
 
 const App = () => {
   return (
@@ -28,12 +32,18 @@ const App = () => {
         <Route component={ContactUs} exact path="/contact-us" />
         <Route component={SignUp} exact path="/signup" />
         <Route component={Home} exact path="/" />
-        <Route component={SignUp} exact path="/signup" />
+        <Route component={NewDeliveryOrder} exact path="/new-delivery-order" />
+        <Route
+          component={DeliveryOrderSummary}
+          exact
+          path="/delivery-order-summary"
+        />
         <Route component={Dashboard} exact path="/admin_dashboard" />
         <Route component={Users} exact path="/admin_users" />
         <Route component={Parcels} exact path="/admin_parcels" />
         <Route component={Orders} exact path="/admin_orders" />
         <Route component={NotFound} path="**" />
+
         <Redirect to="**" />
       </Switch>
     </Router>
@@ -44,5 +54,5 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('app')
+  document.getElementById("app")
 );
